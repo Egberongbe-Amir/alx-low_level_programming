1e54659feb35e3c21c7c17f0a20e4131235c08fb
Egberongbe-Amir <olatunbosunamir@gmail.com>
2022-06-03 15:36:30 -0700

    work

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Prints all single digits in base 10
+* only using putchar
+* Return: Always 0.
+*/
+int main(void)
+{
+int val;
+
+for (int = 0; int < 10; int++)
+putchar((int % 10) + '0');
+
+putchar('\n');
+
+return(0);
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Prints all the numbers of base 16 in lowercase
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+int fig;
+char letter;
+
+for (fig = 0; fig < 10; fig++)
+putchar((num % 10) + '0');
+
+for (letter = 'a'; letter = 'f'; letter++)
+putchar(letter);
+putchar('\n');
+
+return (0);
+}
