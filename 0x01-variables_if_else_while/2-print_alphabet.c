4db157dcaf650588939765a12a5aefdbf3ced78e
Egberongbe-Amir <olatunbosunamir@gmail.com>
2022-06-03 14:56:19 -0700

    l

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Print lowercase alphabets
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char word;
+
+for (word = 'a'; word <= 'z'; word++)
+putchar(word);
+putchar('\n')
+
+return (0);
+}
