2f9b19115beb30e331f1b637dfdabcb70424f3db
Egberongbe-Amir <olatunbosunamir@gmail.com>
2022-06-09 21:26:11 -0700

    sd

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,11 +12,11 @@ int i;
 
 for (i = 0, i <= 9; i++)
 {
-if ((i == 2 || (i == 4)
+if ((i == 2 || (i == 4))
 continue;
 else
-_putchar(i + '0')
+_putchar(i + '0');
 
 }
-_putchar('\n')
+_putchar('\n');
 }
