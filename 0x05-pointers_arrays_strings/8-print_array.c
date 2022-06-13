ae5462339e89dd73be6943f4bea9800bca3f6590
Egberongbe-Amir <64702532+Egberongbe-Amir@users.noreply.github.com>
2022-06-14 00:19:44 +0100

    8date

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -13,10 +13,10 @@ void print_array(int *a, int n)
 int index;
 for (index = 0; index < n; index++)
 {
-printf("%d", a[index]);     
+printf("%d", a[index]);
 if (index == n - 1)
 continue;
 printf(", ");
 }
-printf("\n"); 
+printf("\n");
 }
