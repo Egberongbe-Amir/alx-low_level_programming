2ba9bd16806f2564a4ab6e71fde3ab46638884e7
Egberongbe-Amir <olatunbosunamir@gmail.com>
2022-06-13 10:12:42 -0700

    hul

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -14,13 +14,14 @@ void print_rev(char *s)
 int i, len, pick;
 len = strlen(s);
 
-for (i = 0; i < (len / 2) - 1; i++)
+for (i = 0; i < (len / 2); i++)
 {
 pick = s[i];
 s[i] = s[len - i - 1];
 s[len - i - 1] = pick;
 
 }
-printf("%s\n", s);
 
+printf("%s\n", s)
+return (0);
 }
