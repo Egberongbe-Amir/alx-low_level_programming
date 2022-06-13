a95c7e9484d1d579355541ee83f233c3b53c0588
Egberongbe-Amir <64702532+Egberongbe-Amir@users.noreply.github.com>
2022-06-14 00:17:22 +0100

    Further update

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,10 +11,10 @@ int i = 0, len = 0, j;
 while (str[i++])
 len++;
 if ((len % 2) == 0)
-j = len / 2; 
+j = len / 2;
 else
 j = (len + 1) / 2;
 for (i = j; i < len; i++)
 _putchar(str[i]);
-_putchar('\n');  
+_putchar('\n');
 }
