57788f8a60925e5990f6d8f23ca157e7cd144d43
Egberongbe-Amir <olatunbosunamir@gmail.com>
2022-06-13 03:03:48 -0700

    hull

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 /**
- * rest_to_98 - A function that reset a value to 98
- * @*n: *n is an integer
+ * reset_to_98 - A function that reset a value to 98
+ * @n: *n is an adress of an integer
  *
  * Return: 0
  */
