f1a508b163a6ab9b61387b4e6ade59777d85ae8e
Egberongbe-Amir <olatunbosunamir@gmail.com>
2022-06-13 05:59:09 -0700

    string length@

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - Prints the len of a string
+ * @s: The memory address of string
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _strlen(char *s)
+{
+int k = strlen(s);
+printf("%d\n", k);
+
+return (0);
+}
