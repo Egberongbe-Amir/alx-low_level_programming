43afe7ae1cd7218127c6a026ce1e06d10ff925e0
Egberongbe-Amir <olatunbosunamir@gmail.com>
2022-06-13 12:13:02 -0700

    lok

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * rev_string - Reverse string
+ * @s: string to be reserved
+ */
+
+void rev_string(char *s)
+{
+int i;
+int len;
+int pick;
+
+len = strlen(s);
+for (i = 0; i < (len - 1) / 2; i++)
+{
+pick = s[i];
+
+s[i] = s[len - i - 1];
+s[len - i - 1] = pick;
+}
+}
