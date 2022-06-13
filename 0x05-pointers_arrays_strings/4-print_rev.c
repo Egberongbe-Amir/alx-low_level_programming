b26ad32c427656d5d38b34c748214d13cf6a4fdf
Egberongbe-Amir <olatunbosunamir@gmail.com>
2022-06-13 07:58:55 -0700

    roller

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_rev - Prints the reverse of a string
+ * @s: String address to be reversed
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+int i, len, pick;
+len = strlen(s);
+
+for (i = 0; i < len / 2; i++)
+{
+pick = s[i];
+s[i] = s[len - i - 1];
+s[len - i - 1] = pick;
+}
+
+printf("%s", s);
+return;
+}
