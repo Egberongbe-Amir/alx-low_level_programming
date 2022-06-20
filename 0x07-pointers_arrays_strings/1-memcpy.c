831916893665312c8b61dcb48306048ecb7016cf
Egberongbe-Amir <olatunbosunamir@gmail.com>
2022-06-20 15:30:03 -0700

    first commit

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _memcpy - function copies n bytes from memory area src to memory area dest
+ * @s: memory area
+ * @b: constant byte
+ * @n: first n byte to be filled
+ *
+ * Return: Always Zero
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+memset(s, b, n);
+puts(s);
+}
+
