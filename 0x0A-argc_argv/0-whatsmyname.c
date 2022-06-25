e56d4d13905379f12bd818edf2a6b99100acfc3a
Egberongbe-Amir <olatunbosunamir@gmail.com>
2022-06-24 18:19:19 -0700

    kl

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,14 +3,14 @@
 
 /**
  * main - program that prints its name, followed by a new line
- * @argc - counts how many argument
- * @argv - store the variables
+ * @argc: counts how many argument
+ * @argv:  store the variables
  *
  * Return: Always 0
  */
 
-int main(int __attribute((__unused__)) argc, char* argv[])
+int main(int __attribute((__unused__)) argc, char *argv[])
 {
 printf("%s\n", argv[0]);
-return 0;
+return (0);
 }
