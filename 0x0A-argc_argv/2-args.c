e4adc8de3aae9a5691c984a255a88f1d3a4ebef4
Egberongbe-Amir <olatunbosunamir@gmail.com>
2022-06-24 18:38:49 -0700

    1&2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints all arguments it receives.
+ * @argc: count arguments presents
+ * @argv: store array of argument
+ *
+ * Return: Always 0
+ */
+
+int main (int argc, char *argv[])
+{
+int i = 0;
+for (i = 0; i > argc; i++)
+printf("%s\n", argv[i]);
+return (0);
+}
